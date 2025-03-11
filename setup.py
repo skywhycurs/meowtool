4e0a389dd11d4a07574d7417e67e599d0ac99bb3
skywhycurs <zxcshade45@gmail.com>
2025-03-11 19:16:05 +0700

    setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 import os
 
 def system():
-  pc = input("выбери свою систему: \n  1.Linux \n  2.Termux")
+  pc = input("выбери свою систему: \n  1.Linux \n  2.Termux \n")
   if pc == 1:
       os.system("pip install -r requiments.txt")
       print("в последующих запусках используйте: python3 main.py")
@@ -12,7 +12,7 @@ def system():
       
                 
 
-if __name__ == "__setup__":
+if __name__ == "__main__":
   system()
   print("приятного использования")
   os.system("python3 main.py")
