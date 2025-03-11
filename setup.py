6431d04c092dcb8139cf83852e5ae960eb0a821b
skywhycurs <zxcshade45@gmail.com>
2025-03-11 19:11:15 +0700

    setup.py

---
+++ setup.py
@@ -0,0 +1,19 @@
+import os
+
+def system():
+  pc = input("выбери свою систему: \n  1.Linux \n  2.Termux")
+  if pc == 1:
+      os.system("pip install -r requiments.txt")
+      print("в последующих запусках используйте: python3 main.py")
+      
+  if pc == 2:
+      os.system("pip install -r requiments.txt")
+      print("в последующих запусках используйте: python3 main.py")
+      
+                
+
+if __name__ == "__setup__":
+  system()
+  print("приятного использования")
+  os.system("python3 main.py")
+  
